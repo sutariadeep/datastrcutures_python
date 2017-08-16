13ee009b868d5a69889eb18c18b8431adb4cac53
sutariadeep <sutariadeep@gmail.com>
2017-08-16 11:45:13 -0700

    Permutations of a string recursively

--- ArdenDertat/permutations_string.py
+++ ArdenDertat/permutations_string.py
@@ -1,9 +1,9 @@
-def permutations(word):
+def permutations1(word):
     if len(word) ==1:
         return [word]
  
     #get all permutations of length N-1
-    perms=permutations(word[1:])
+    perms=permutations1(word[1:])
     char=word[0]
     result=[]
     #iterate over all permutations of length N-1
@@ -13,4 +13,29 @@ def permutations(word):
             result.append(perm[:i] + char + perm[i:])
     return result
 
-print permutations('abc')
+# Python program to print all permutations with
+# duplicates allowed using recursion
+ 
+def toString(List):
+    return ''.join(List)
+ 
+# Function to print permutations of string
+# This function takes three parameters:
+# 1. String
+# 2. Starting index of the string
+# 3. Ending index of the string.
+def permutaions2(a, l, r):
+    if l==r:
+        print toString(a)
+    else:
+        for i in xrange(l,r+1):
+            a[l], a[i] = a[i], a[l]
+            permutaions2(a, l+1, r)
+            a[l], a[i] = a[i], a[l] # backtrack
+ 
+# Driver program to test the above function
+print permutations1('abc')
+string = "ABC"
+n = len(string)
+a = list(string)
+permutaions2(a, 0, n-1)
